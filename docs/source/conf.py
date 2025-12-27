ad417676730f9f857a24efc31add3c4c1c8fd5b2
Treyvon <ptravon@gmail.com>
2025-12-27 00:14:24 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
+project = 'Force System Lite'
 copyright = '2021, Graziella'
-author = 'Graziella'
+author = 'Project Star Forge'
 
 release = '0.1'
 version = '0.1.0'
